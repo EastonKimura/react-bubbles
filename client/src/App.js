5a8d934df22436ebd4073be44649017cf86b16c6
unknown <eastkim1@hotmail.com>
2020-02-28 11:51:27 -0700

    MVP questions are in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,3 +17,13 @@ function App() {
 }
 
 export default App;
+
+//Explain what a token is used for.
+//used to protect something and acts like an electronic key
+//- [ ] What steps can you take in your web apps to keep your data secure?
+//back up your data, scan for vulnerabilities, keep everything up to date and ask people to test it
+//- [ ] Describe how web servers work.
+//processes incoming netowrk requests over HTTP and several other related protocols.
+//- [ ] Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+//POST, GET, PUT, PATCH, DELETE. they create, read, update/replace, update/modify, and delete.
+
